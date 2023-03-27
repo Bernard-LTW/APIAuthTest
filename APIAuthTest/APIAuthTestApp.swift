7c3c9fd6127a4876641aece2b5edc3e673f6f3d9
Bernard-LTW <34589202+Bernard-LTW@users.noreply.github.com>
2023-03-28 08:28:23 +0900

    Initial Commit

---
+++ APIAuthTest/APIAuthTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIAuthTestApp.swift
+//  APIAuthTest
+//
+//  Created by Bernard Lee on 28/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct APIAuthTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
